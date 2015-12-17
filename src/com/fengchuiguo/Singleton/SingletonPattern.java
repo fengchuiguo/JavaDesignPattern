200b5616a3a4fd527977e23c9721e7376d055798
liuhaiyuan <1028471848@qq.com>
2015-12-17 11:52:10 +0800

    update

---
+++ src/com/fengchuiguo/Singleton/SingletonPattern.java
@@ -0,0 +1,12 @@
+package com.fengchuiguo.Singleton;
+
+/**
+ * Created by Administrator on 2015/12/17 0017.
+ */
+public class SingletonPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Hello SingletonPattern!");
+    }
+
+}
